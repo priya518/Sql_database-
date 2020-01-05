a296318ca1086c02297598179a639abaf421e22c
Priya Patel <Pp520942@gmail.com>
2020-01-05 11:15:40 +0530

    Initial Commit

---
+++ SqlExample1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SqlExample1
+//
+//  Created by Priya patel on 05/01/20.
+//  Copyright © 2020 Priya patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SqlExample1Tests/SqlExample1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  SqlExample1Tests.swift
+//  SqlExample1Tests
+//
+//  Created by Priya patel on 05/01/20.
+//  Copyright © 2020 Priya patel. All rights reserved.
+//
+
+import XCTest
+@testable import SqlExample1
+
+class SqlExample1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
